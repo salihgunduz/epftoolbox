d91a3ccc7e469ffb460add9e7b3e253f6c97f0c4
salihgunduz <gunduz.salih@gmail.com>
2020-09-10 09:45:38 +0300

    hyperopt_objective
    path_hyperparameters_folder is wong parameter for hyperopt_objective.hyperparameter_optimizer works after it is cutted.

--- examples/optimizing_hyperparameters_dnn.py
+++ examples/optimizing_hyperparameters_dnn.py
@@ -14,7 +14,7 @@ nlayers = 2
 
 # Market under study. If it not one of the standard ones, the file name
 # has to be provided, where the file has to be a csv file
-dataset = 'NP'
+dataset = 'PJM'
 
 # Number of years (a year is 364 days) in the test dataset.
 years_test = 2
@@ -23,8 +23,8 @@ years_test = 2
 # the test dataset is built using the years_test parameter. They should either be one of
 # the date formats existing in python or a string with the following format
 # "%d/%m/%Y %H:%M"
-begin_test_date = None
-end_test_date = None
+begin_test_date = '27/12/2016'
+end_test_date = '01/03/2017'
 
 # Boolean that selects whether the validation and training datasets are shuffled
 shuffle_train = 1
